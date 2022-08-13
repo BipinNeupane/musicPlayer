7deb52192c2226dfbf4c49f4124d0c3371caeba6
BipinNeupane <bipin2022121@nami.edu.np>
2022-08-13 15:25:21 +0545

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const playMusic = document.getElementById("playBtn");
 const music = document.querySelector("audio");
 const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const next = document.getElementById("nextBtn");
+const previous = document.getElementById("previousBtn");
+
+// creating an array list for song titles, artists
+const songList = [
+    {
+        track : "vivaLaVida",
+        name : "Viva La Vida",
+        artist : "Coldplay"
+    },
+
+    {
+        track : "goldn",
+        name : "Goldn",
+        artist : "prazkhanal"
+    },
+    {
+        track : "lofi",
+        name : "Lofi Study",
+        artist : "FASSound"
+    }
+];
+
 
 let isPlaying = false;
 
@@ -21,3 +46,34 @@ const pauseSong = ()=>{
 playMusic.addEventListener("click", () =>{
     isPlaying ? pauseSong() : playSong();
 });
+
+const loadSong = (songList) => {
+    title.textContent = songList.name;
+    artist.textContent = songList.artist;
+    music.src = `music/${songList.track}.mp3`
+    image.src = `src/${songList.track}.png`
+}
+
+let songIndex = 0;
+// loadSong(songList[2]);
+
+// making the nextSong button work
+const nextSong = () => {
+    // incrementing the song index
+    songIndex = (songIndex + 1)% songList.length;
+    // if songindex is 0 0+1 % 3 = 1
+    loadSong(songList[songIndex]);
+    playSong();
+}
+
+const prevSong = () => {
+    // decrementing the song index
+    songIndex = (songIndex - 1 + songList.length);
+    // if songIndex is 0 0-1 + 3 = 2 
+    loadSong(songList[songIndex]);
+    playSong();
+}
+
+
+next.addEventListener("click",nextSong);
+previous.addEventListener("click", prevSong);
